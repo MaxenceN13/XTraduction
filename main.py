c2f2b473cd08c5b4847c3b4d877702820c8dce9a
Maxence Nayet <m.nayet201@gmail.com>
2022-06-06 00:27:37 +0200

    ✅ Add searching for Tweets

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import twitterengine
 
 if __name__ == "__main__":
-    print("⭐ Lancement de TradInstant ⭐")
-    print("Authentification de l'utilisateur")
-    Client = twitterengine.LoginOnTwitter()
+	print("⭐ Lancement de TradInstant ⭐")
+	print("Authentification de l'utilisateur")
+	Client = twitterengine.LoginOnTwitter()
+	MentionsList = twitterengine.SearchRecentMentions(Client)
--- twitterengine.py
+++ twitterengine.py
@@ -2,7 +2,21 @@ import config
 import tweepy
 
 def LoginOnTwitter():
-    return tweepy.Client(consumer_key=config.API_KEY,
-			     consumer_secret=config.API_SECRET,
-			     access_token=config.ACCESS_TOKEN,
-			     access_token_secret=config.ACCESS_TOKEN_SECRET)
+	return tweepy.Client(consumer_key=config.API_KEY,
+				 consumer_secret=config.API_SECRET,
+				 access_token=config.ACCESS_TOKEN,
+				 access_token_secret=config.ACCESS_TOKEN_SECRET)
+
+## Requetes faites a Twitter
+
+def SearchRecentMentions(ClientTweepy):
+	TweetList = ClientTweepy.search_recent_tweets(query="is:quote @elon_musk_fr",
+												  max_results=10,
+												  user_auth=1)
+	print(TweetList)
+	return TweetList
+
+def GetQuotedTweet(Client, TweetId):
+	QuoteTweet = Client.get_quote_tweets(TweetId)
+	print(f"Quote Tweet récupéré : {QuoteTweet}")
+	return QuoteTweet
