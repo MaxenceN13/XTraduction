eb94f1aefe7ab2d683ce829812faff1894630d3e
Maxence Nayet <m.nayet201@gmail.com>
2022-06-06 03:03:30 +0200

    Répond au tweet initial avec la traduction du tweet

--- twitterengine.py
+++ twitterengine.py
@@ -1,26 +1,29 @@
 import config
 import tweepy
-import re
+import translateDeeplAPI
+
 
 class TweetPrinterV2(tweepy.StreamingClient):
-	
+	#Fonction Asyncrone (Declenché par la rule qui detecte les réponses) : Elle extrait le tweet initiant & le tweet a traduire,
+	# puis elle tweet la reponse
 	def on_response(self, response):
 		initialTweet = response.data
 		tweetToTranslate = response.includes['tweets'][0]
 		print(initialTweet.text)
 		print(tweetToTranslate.text)
-		# print(f"{tweet.id} {tweet.created_at} ({tweet.author_id}): {tweet.text}")
-		# print("-"*50)
+		client = getClient()
+		translatedTweet = translateDeeplAPI.Translate(tweetToTranslate.text)
+		AnswerToTweet(client, translatedTweet, initialTweet.id)
  
 
 
-def LoginOnTwitter():
+def getClient():
 	return tweepy.Client(consumer_key=config.API_KEY,
 						 consumer_secret=config.API_SECRET,
 						 access_token=config.ACCESS_TOKEN,
 						 access_token_secret=config.ACCESS_TOKEN_SECRET)
 
-# Requetes faites a Twitter
+
 
 def getStreamingClient():
 	printer = TweetPrinterV2(config.BEARER_TOKEN)
@@ -28,13 +31,12 @@ def getStreamingClient():
 	printer.add_rules(rule)
 	printer.filter(expansions=["referenced_tweets.id"])
 
+
 def SearchRecentMentions(ClientTweepy):
 	TweetList = ClientTweepy.search_recent_tweets(query="(is:quote OR is:reply) @elon_musk_fr",
 												  max_results=10,
-												  expansions=[
-													  "referenced_tweets.id"],
+												  expansions=["referenced_tweets.id"],
 												  user_auth=1)
-	# print(TweetList)
 	return TweetList
 
 
