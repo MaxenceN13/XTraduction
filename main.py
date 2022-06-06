a65a79a8ec08f46ba0bbde0164acb549db1dea71
Maxence Nayet <m.nayet201@gmail.com>
2022-06-06 03:02:29 +0200

    Répond au tweet initial avec la traduction

--- main.py
+++ main.py
@@ -3,19 +3,5 @@ import translateDeeplAPI
 
 if __name__ == "__main__":
     print("⭐ Lancement de TradInstant ⭐")
-    print("Authentification de l'utilisateur")
-    """Client = twitterengine.LoginOnTwitter()
-    MentionsList = twitterengine.SearchRecentMentions(Client)
-
-    NewLastTweetId = MentionsList.data[0].id
-
-    LastTweetId = 0
-
-    # Si on trouve un nouveau tweet non traité
-    if NewLastTweetId != LastTweetId:
-        TweetToTranslate = MentionsList.includes['tweets'][0].text
-        TweetTranslated = translateDeeplAPI.Translate(TweetToTranslate)
-        twitterengine.AnswerToTweet(Client, TweetTranslated, NewLastTweetId)"""
-    
-    Client = twitterengine.LoginOnTwitter()
     twitterengine.getStreamingClient()
+    print("Extinction du ")
