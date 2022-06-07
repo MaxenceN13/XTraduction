988a0e4f025d1d2030f7e8b3fb1f86e455f91e83
Maxence Nayet <m.nayet201@gmail.com>
2022-06-07 11:04:31 +0200

    Transfère la fonction de on_repsonse dans le mainv via callback_function

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-import twitterengine
+import myStreamingClient
+import tweepy
+import translateDeeplAPI
+import config
+
+# Recupère un client object (permet d'effectuer des actions sur un compte comme tweeter par exemple)
+def getClient():
+	return tweepy.Client(consumer_key=config.API_KEY,
+						 consumer_secret=config.API_SECRET,
+						 access_token=config.ACCESS_TOKEN,
+						 access_token_secret=config.ACCESS_TOKEN_SECRET)
+
+# Post un tweet avec le compte Client en réponse à un TweetId avec comme contenue le TweetText
+def answerToTweet(Client, TweetText, IdSourceTweet):
+	Client.create_tweet(text=TweetText,
+						in_reply_to_tweet_id=IdSourceTweet,
+						user_auth=1)
 
 if __name__ == "__main__":
-    print("⭐ Lancement de TradInstant ⭐")
-    twitterengine.startListeningNewTweetWithSpecificRules()
-    print("Programme effectué avec succès !")
+	print("⭐ Lancement de TradInstant ⭐")
+
+	def tweetTranslatedText(response):
+		# Recupère le tweet qui à initier l'évenement
+		initialTweet = response.data
+		# Recupère le tweet cité ou répondu
+		tweetToTranslate = response.includes['tweets'][0]
+		# Recupère le client (pour effectuer des actions sur le compte Twitter)
+		client = getClient()
+		# Traduit le texte du tweet
+		translatedTweet = translateDeeplAPI.translate(tweetToTranslate.text)
+		# Répond aux tweet initial avec la traduction du tweet
+		answerToTweet(client, translatedTweet, initialTweet.id)
+	
+	client = getClient()
+
+	streaming_client = myStreamingClient.myStreamingClient(config.BEARER_TOKEN, tweetTranslatedText)
+
+	streaming_client.filter(expansions=["referenced_tweets.id"])
+
+	print("Programme effectué avec succès !")
--- translateDeeplAPI.py
+++ translateDeeplAPI.py
@@ -4,7 +4,7 @@ import config
 auth_key = config.DEEPL_AUTH_KEY
 translator = deepl.Translator(auth_key)
 
-def Translate(SourceText, lang = "FR"):
+def translate(SourceText, lang = "FR"):
 	result = translator.translate_text(SourceText, target_lang=lang)
 	print(f"Tweet Traduit de '{SourceText}' à '{result.text}'") 
 	return result.text
